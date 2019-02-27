bb3c21df334f00657339e0d44e5a9fa378832cb6
Lorenzo Peppoloni <lorenzo@Lorenzos-Air.lan>
2019-02-27 08:03:38 +0000

    modified for multilabels

--- src/correlations.py
+++ src/correlations.py
@@ -1,19 +1,46 @@
 import pandas as pd
 import sys
+from scipy.stats import pearsonr, kendalltau, spearmanr
 
 first_file = sys.argv[1]
 second_file = sys.argv[2]
 
-def corr(first_file, second_file):
-  first_df = pd.read_csv(first_file,index_col=0)
-  second_df = pd.read_csv(second_file,index_col=0)
+TRAIN_CSV_PATH = "/Users/lorenzo/kaggle/whales/dataset/train.csv"
+
+
+
+def corr(first_file, second_file, labels_to_idx):
+  first_df = pd.read_csv(first_file)
+  second_df = pd.read_csv(second_file)
   # assuming first column is `prediction_id` and second column is `prediction`
-  prediction = first_df.columns[0]
+  prediction = first_df.columns[1]
+  
+  first_prediction = []
+  second_prediction = []
+
+  for _, row in first_df.iterrows():
+    pred = row[prediction]
+    labels_list = pred.split(" ")
+    if len(labels_list) < 5:
+      print(labels_list)
+    first_prediction.extend([labels_to_idx[l] for l in labels_list])
+
+  for _, row in second_df.iterrows():
+    pred = row[prediction]
+    labels_list = pred.split(" ")
+    if len(labels_list) < 5:
+      print(labels_list)
+    second_prediction.extend([labels_to_idx[l] for l in labels_list])
+  
   # correlation
   print("Finding correlation between: {} and {}".format(first_file,second_file))
   print("Column to be measured: {}".format(prediction))
-  print("Pearson's correlation score: {}".format(first_df[prediction].corr(second_df[prediction],method='pearson')))
-  print("Kendall's correlation score: {}".format(first_df[prediction].corr(second_df[prediction],method='kendall')))
-  print("Spearman's correlation score: {}".format(first_df[prediction].corr(second_df[prediction],method='spearman')))
+  print("Pearson's correlation score: {}".format(pearsonr(first_prediction, second_prediction)))
+  print("Kendall's correlation score: {}".format(kendalltau(first_prediction, second_prediction)))
+  print("Spearman's correlation score: {}".format(spearmanr(first_prediction, second_prediction)))
 
-corr(first_file, second_file)
+
+train_df = pd.read_csv(TRAIN_CSV_PATH)
+labels_to_idx = {label: i for i, label in enumerate(sorted(train_df["Id"].unique()))}
+
+corr(first_file, second_file, labels_to_idx)
